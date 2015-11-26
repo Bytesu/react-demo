2ffb8503fb312a86f66a9eea383b1a3976e5723c
苏伟明 <byte_su@163.com>
2015-11-26 10:30:58 +0800

    demo tutorial

---
+++ test/hello.build.js
@@ -0,0 +1,8 @@
+/**
+ * Created by s_ on 15/11/25.
+ */
+ReactDOM.render(React.createElement(
+  'h1',
+  null,
+  'Hello , world ! '
+), document.getElementById('example'));
---
+++ test/hello.jsx
@@ -0,0 +1,7 @@
+/**
+ * Created by s_ on 15/11/25.
+ */
+ReactDOM.render(
+<h1>Hello , world ! </h1>,
+    document.getElementById('example')
+);
